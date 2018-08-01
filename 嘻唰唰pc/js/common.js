52ed290f2c25a72b35ef075ab375fe988ee6eb18
dzw9958 <122418047@qq.com>
2018-08-01 15:42:39 +0800

    dzw 8/1 15

---
+++ 嘻唰唰pc/js/common.js
@@ -0,0 +1,12 @@
+//头顶显示隐藏二维码
+var code = $(".top_contain .top_hide");
+$(".top_contain .top_btn").hover(function(){
+    code.show();
+}, function(){
+    code.hide();
+});
+code.hover(function(){
+    code.show();
+}, function(){
+    code.hide();
+});
--- 嘻唰唰pc/js/index.js
+++ 嘻唰唰pc/js/index.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-    //头顶显示隐藏二维码
-    var code = $(".top_contain .top_hide");
-    $(".top_contain .top_btn").hover(function(){
-        code.show();
-    }, function(){
-        code.hide();
-    });
-    code.hover(function(){
-        code.show();
-    }, function(){
-        code.hide();
-    });
-
     //banner轮播
     jQuery(".banner_contain").slide({
         mainCell: ".bd ul",
@@ -19,7 +6,7 @@ $(document).ready(function(){
         autoPlay: true
     });
 
-    //键盘并非奢想
+    //健康并非奢想
     jQuery(".health_wheel").slide({
         mainCell: ".bd ul",
         autoPage: true,

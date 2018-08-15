fd2631dc4f2348ea5cf86996157a0f021badc4db
dzw9958 <122418047@qq.com>
2018-08-15 11:56:47 +0800

    dzw 8/15-12

---
+++ 妙自然/js/index.js
@@ -0,0 +1,39 @@
+	
+var runPage, interval, autoPlay, thisPage;
+
+autoPlay = function(to) {
+    clearTimeout(interval);
+    interval = setTimeout(function() {
+        runPage.go(to);
+    }, 10000);
+}
+
+runPage = new FullPage({
+    id: 'pageContain',                            // id of contain
+    slideTime: 800,                               // time of slide
+    continuous: true,
+    effect: {                                     // slide effect
+        transform: {
+            translate: 'Y',					      // 'X'|'Y'|'XY'|'none'
+            scale: [1, 1],					      // [scalefrom, scaleto]
+            rotate: [0, 0]					      // [rotatefrom, rotateto]
+        },
+        opacity: [0, 1]                           // [opacityfrom, opacityto]
+    },
+    mode: 'touch,nav:navBar',                     // mode of fullpage
+    easing: [0, .93, .39, .98],                   // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1] )
+    callback : function(index, thisPage) {     // callback when pageChange
+        index = index + 1 > 7 ? 0 : index + 1;
+        autoPlay(index);
+    }
+});
+
+interval = setTimeout(function() {
+    runPage.go(runPage.thisPage() + 1);
+}, 10000);
+
+// 导航切换
+$(".nav .nav_btn").on("click", function(e){
+    e.preventDefault();
+    $('.nav').toggleClass('active');
+});

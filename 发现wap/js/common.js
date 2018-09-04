db871c9aec7c8272071277f9928b97a09164f395
dzw9958 <122418047@qq.com>
2018-09-04 14:45:43 +0800

    dzw 9/4-14

---
+++ 发现wap/js/common.js
@@ -0,0 +1,3 @@
+$(".header_search").on("click", function(){
+  $(".header_center input").toggle();
+})
---
+++ 发现wap/js/index.js
@@ -0,0 +1,48 @@
+// banner图轮播
+var mySwiper = new Swiper('.swiper-container',{
+  pagination: '.pagination',
+  paginationClickable: true,
+  autoplay: 3000,
+  loop: true,
+  moveStartThreshold: 100,
+  autoplayDisableOnInteraction: false
+})
+
+// 点赞功能
+$(".friend_likes").on("click", function(){
+  var that = $(this);
+  if(that.hasClass("on")){
+    return alert("你已经点过赞了！");
+  }else{
+    $(this).addClass("on");
+    return alert("点赞成功！");
+  }
+})
+// 点击评论弹出评论框获取/失去焦点
+$(".friend_btn").on("click", function(){
+  $(".comment").toggle();
+  $(".comment_text").focus();
+})
+
+$(".comment").on("click touchstart", function(){
+  $(".comment_text").blur();
+})
+
+$(".comment_text").on("blur", function(){
+  $(".comment").toggle();
+})
+
+// 阻止冒泡事件
+$(".comment_text").on("click touchstart", function(e){
+  e.stopPropagation();
+})
+$(".comment_submit").on("click touchstart", function(e){
+  e.stopPropagation();
+  var text = $(".comment_text").val();
+  if(text == ""){
+    return alert("评论不能为空！");
+  }else{
+    $(".comment_text").val("").blur();
+    return alert("评论成功！");
+  }
+})

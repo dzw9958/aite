979e11d20394f633c34c68c1e3f8b7e229b5c018
dzw9958 <122418047@qq.com>
2018-09-04 15:29:20 +0800

    dzw 9/4-15

--- 发现wap/js/common.js
+++ 发现wap/js/common.js
@@ -1,3 +1,14 @@
+// 顶部导航点击切换搜索
 $(".header_search").on("click", function(){
   $(".header_center input").toggle();
+})
+
+// banner图轮播
+var mySwiper = new Swiper('.swiper-container',{
+  pagination: '.pagination',
+  paginationClickable: true,
+  autoplay: 3000,
+  loop: true,
+  moveStartThreshold: 100,
+  autoplayDisableOnInteraction: false
 })
--- 发现wap/js/index.js
+++ 发现wap/js/index.js
@@ -1,13 +1,3 @@
-// banner图轮播
-var mySwiper = new Swiper('.swiper-container',{
-  pagination: '.pagination',
-  paginationClickable: true,
-  autoplay: 3000,
-  loop: true,
-  moveStartThreshold: 100,
-  autoplayDisableOnInteraction: false
-})
-
 // 点赞功能
 $(".friend_likes").on("click", function(){
   var that = $(this);

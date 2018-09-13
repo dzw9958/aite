7bf47f4cf9feb75da53a2e987246730179e8e7d2
dzw9958 <122418047@qq.com>
2018-09-13 17:48:02 +0800

    dzw 9/13-17

--- 发现套数据/js/common.js
+++ 发现套数据/js/common.js
@@ -1,6 +1,6 @@
 // 创建url链接
 var URL_wap = "http://aitecc.com/mobile/";
-var URL_pc = "http://aitecc.com/"
+var URL_pc = "http://aitecc.com/";
 
 // 顶部导航点击切换搜索
 $(".header_right").on("click", ".header_search", function () {

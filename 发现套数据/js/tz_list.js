c4cbdbc62af6e2af872ca5cbe506f7c3077699c9
dzw9958 <122418047@qq.com>
2018-09-14 16:05:25 +0800

    dzw 9/14-16

--- 发现套数据/js/tz_list.js
+++ 发现套数据/js/tz_list.js
@@ -1,43 +1,56 @@
+// 创建变量
+var type = 1;
 
-/* 下拉加载更多 */
-var type = $(".switch_list .on").data("type");
-var page = 1;
+var page = 2;
 var timers = null;
 
-$(".switch_list").on("click", "li", function(){
+$(".switch_list").on("click", "li", function () {
   var that = $(this);
   type = that.data("type");
   that.addClass("on").siblings().removeClass("on");
+  page = 1;
+  $(".main_list").empty();
+  LoadingDataFn();
 })
 
 //加载数据
 var LoadingDataFn = function () {
-  var dom = '';
-  for (var i = 0; i < 3; i++) {
-    dom += '<li>';
-    dom += '<a href="#">';
-    dom += '<div class="main_text"">';
-    dom += '<h2>在区块链做技术，除了高薪他们还想对抗人性之恶</h2>';
-    dom += '<p>';
-    dom += '<span class="main_time">08/25</span>';
-    dom += '<span class="main_author">CEO观点</span>';
-    dom += '<span class="main_com">1200</span>';
-    dom += '<span class="main_eye">1200</span>';
-    dom += '</p>';
-    dom += '</div>';
-    dom += '<div class="main_img">';
-    dom += '<img src="./img/img.png" alt="">';
-    dom += '</div>';
-    dom += '</a>';
-    dom += '</li>';
-  }
-  $('.main_list').append(dom);
+  $.ajax({
+    type: "get",
+    url: URL_wap + "index.php?act=find_cms_article&op=list",
+    data: {
+      "orderType": type,
+      // "pagesize": 8,
+      "curpage": page
+    },
+    dataType: "json",
+    success: function (res) {
+      page += 1;
+      nextpage = res.datas.is_nextpage;
+      var list = res.datas.list;
+      var str = '';
+      for (var i = 0; i < list.length; i++) {
+        str += '<li>';
+        str += '<a href="' + list[i].url + '">';
+        str += '<div class="main_text"">';
+        str += '<h2>' + list[i].article_title + '</h2>';
+        str += '<p>';
+        str += '<span class="main_time">' + list[i].article_publish_time + '</span>';
+        str += '<span class="main_author">' + list[i].article_publisher_name + '</span>';
+        str += '<span class="main_com">' + list[i].article_comment_count + '</span>';
+        str += '<span class="main_eye">' + list[i].article_click + '</span>';
+        str += '</p>';
+        str += '</div>';
+        str += '<div class="main_img">';
+        str += '<img src="' + list[i].article_image + '" title="' + list[i].article_title + '">';
+        str += '</div>';
+        str += '</a>';
+        str += '</li>';
+      }
+      $(".main_list").append(str);
+    }
+  });
 };
-
-//初始化， 第一次加载
-$(document).ready(function () {
-  LoadingDataFn();
-});
 
 //还可以基window窗口（body）来添加滚动事件, (因为布局不同,所以在这里没效果，因为[上面是基于body中的某个元素来添加滚动事的])
 $(window).scroll(function () {
@@ -45,9 +58,11 @@ $(window).scroll(function () {
   if (($(window).height() + $(window).scrollTop() + 50) >= $(document).height()) {
     clearTimeout(timers);
     timers = setTimeout(function () {
-      page++;
-      console.log("第" + page + "页");
-      LoadingDataFn();
+      if (nextpage == 0) {
+        return alert("没有更多数据了");
+      } else {
+        LoadingDataFn();
+      }
     }, 300);
   }
 });

5eb5f5f296d09c14f020be5d40df960b6ce31f4a
dzw9958 <122418047@qq.com>
2018-09-14 11:00:49 +0800

    dzw 9/14-11

--- 发现套数据/js/common.js
+++ 发现套数据/js/common.js
@@ -2,22 +2,6 @@
 var URL_wap = "http://aitecc.com/mobile/";
 var URL_pc = "http://aitecc.com/";
 
-// 顶部导航点击切换搜索
-$(".header_right").on("click", ".header_search", function () {
-  var that = $(this);
-  var input = $(".header_center input");
-  var href = that.data("href");
-  var key = input.val();
-  if (!input.hasClass("on")) {
-    return input.addClass("on");
-  } else if (key == "") {
-    return input.removeClass("on");
-  } else if (key !== "") {
-    that.attr("href", href + "&key=" + key);
-    input.val("");
-  }
-})
-
 // banner图轮播
 function switchImg() {
   this.name = new Swiper('.swiper-container', {
